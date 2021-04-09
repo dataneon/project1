ae395cf6407e15629c9aee033c9e14c9618196cd
lousteez <louissterrett@gmail.com>
2021-04-09 16:59:09 -0500

    Trying to write function that returns 3 unique items in an array for the sake of propogating buttons

--- script.js
+++ script.js
@@ -30,7 +30,9 @@ let wrongAnswers = [
     "Pine",
     "Apples",
     "Rose",
-    "Aquinas"
+    "Aquinas",
+    "Rabbits",
+    "Snake oil"
 ]
 
 // define buttons
@@ -71,16 +73,94 @@ function newQuestion() {
     let currentName = currentPlant.name
     let currentImgSrc = currentPlant.imageSource
     let currentFamily = currentPlant.family
-    // console.log(currentImgSrc)
-
-    console.log(triviaQuestions[randArrIndex].family)
-
 
     // load image
+
     // load correct answer into one of the buttons
+    let answerArray = [null, null, null, null]
+
+    // random num for placement of answer in answerArray
+    let randAnsIndex = Math.floor(Math.random() * answerArray.length)
+
+    // place the currentFamily variable randomly in the answerArray
+    answerArray[randAnsIndex] = currentFamily
+
+    // returns an array of 3 nonidentical, wrong answers
+    // function fillCurrentWrongAnswers(){
+    //     function rFill (i, currentWrongAnswers) {
+    //         if (currentWrongAnswers.includes(null) == false) {
+    //             return currentWrongAnswers
+    //         } else {
+    //             let randWrongAnswer = wrongAnswers[Math.floor(Math.random() * wrongAnswers.length)]
+    //             if ((currentWrongAnswers[i] == null) && 
+    //                 (currentWrongAnswers.includes(randWrongAnswer) == false)) {
+    //                 currentWrongAnswers[i] = randWrongAnswer
+    //                 return rFill(i + 1, currentWrongAnswers)
+    //             }
+    //         }
+    //     }
+    //     return rFill(0, [null, null, null])
+    // }
+
+    // make array of 3 wrong answers that are nonidentical
+    // currentWrongAnswers = fillCurrentWrongAnswers()
+
+    // console.log(`currentWrongAnswers: ${currentWrongAnswers}`)
+
+    // add the incorrect answers to the answerArray
+    // this sometimes works and sometimes causes an error
+    // I think the error is caused by somehow removing too many items from the list
+    // for (let i = 0; i < answerArray.length; i++) {
+    //     if (answerArray[i] == null) {
+    //         answerArray[i] = currentWrongAnswers[currentWrongAnswers.length - 1]
+    //         currentWrongAnswers.pop()
+    //     }
+    // }
+
+    console.log(answerArray)
+
     // load wrong answers into the remaining buttons
 }
 
+function threeWrongAnswers(){
+    function rThree (wrongArr) {
+        if (wrongArr.length = 3) {
+            return wrongArr
+        } else {
+            let randWrongAnswerIndex = Math.floor(Math.random() * wrongAnswers.length)
+            let randWrongAnswer = wrongAnswers[randWrongAnswerIndex]
+            if (wrongArr.includes(randWrongAnswer) == false) {
+                wrongArr.push(randWrongAnswer)
+                return rThree(wrongArr)
+            } else if (wrongArr.includes(randWrongAnswer) == true) {
+                return rThree(wrongArr)
+            }
+        }
+    }
+    return rThree([])
+}
+
+// trying to rewrite threeWrongAnswers() in the most basic way
+function threeNonidenticalItems(){
+    let myArr = ["a", "b", "c", "d", "e", "f", "g"]
+    function rThree (newArr) {
+        if (newArr.length == 3) {
+            return newArr
+        } else {
+            let randIndex = Math.floor(Math.random() * myArr.length)
+            let randItem = myArr[randIndex]
+            if (newArr.includes(randItem) == false) {
+                newArr.push(randItem)
+                return rThree(newArr)
+            } else if (newArr.includes(randItem) == true) {
+                return rThree(newArr)
+            } else {
+                console.log("something went wrong")
+            }
+        }
+    }
+    rThree([])
+}
 
 // get API information
 
