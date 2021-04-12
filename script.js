38b9052486820a8f65432efb336d763dd9b6efa4
lousteez <louissterrett@gmail.com>
2021-04-12 18:59:44 -0500

    added more elements to triviaQuestions object, including images

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
 // define DOM elements
-const answerButtons = document.querySelector(".answerButtons")
+let gameResponse = document.querySelector("#gameResponse")
+let answerButtons = document.querySelector(".answerButtons")
 let currentImage = document.querySelector("#currentPlantImage")
 let plantName = document.querySelector("#plantName")
 let scoreSpan = document.querySelector("#scoreSpan")
 let scoreNum = 0
 
 // define object of questions with their answers
-const triviaQuestions = [
-    {"name": "pine tree",
+let triviaQuestions = [
+    {"name": "a pine tree",
     "family": "Pinaceae",
     "imageSource": "img/pine.jpg"},
-    {"name": "palm tree",
+    {"name": "a palm tree",
     "family": "Arecaceae",
     "imageSource": "img/palm.jpg"},
-    {"name": "joshua tree",
+    {"name": "a joshua tree",
     "family": "Asparagaceae",
     "imageSource": "img/yucca.jpg"},
-    {"name": "baobab tree",
+    {"name": "a baobab tree",
     "family": "Malvaceae",
     "imageSource": "img/baobab.jpg"},
-    {"name": "oak tree",
+    {"name": "an oak tree",
     "family": "Fagaceae",
     "imageSource": "img/oak.jpg"},
-    {"name": "bog birch tree",
+    {"name": "a bog birch tree",
     "family": "Betulaceae",
     "imageSource": "img/bogbirch.jpg"},
-    {"name": "beavertail pricklypear",
-    "family": "Cactaceae",
-    "imageSource": "img/beavertail.jpg"},
+    {"name": "a poppy",
+    "family": "Papaveraceae",
+    "imageSource": "img/poppy.jpg"},
+    {"name": "a bamboo tree",
+    "family": "Poaceae",
+    "imageSource": "img/bamboo.jpg"},
+    {"name": "wisteria",
+    "family": "Fabaceae",
+    "imageSource": "img/wisteria.jpg"},
+    {"name": "rainbow eucalyptus",
+    "family": "Myrtaceae",
+    "imageSource": "img/eucalyptus.jpg"},
+    {"name": "a willow tree",
+    "family": "Salicaceae",
+    "imageSource": "img/willow.jpg"},
+    {"name": "a maple tree",
+    "family": "Sapindaceae",
+    "imageSource": "img/maple.jpg"},
+    {"name": "poison ivy",
+    "family": "Anacardiaceae",
+    "imageSource": "img/poisonivy.jpg"},
+    {"name": "aloe",
+    "family": "Asphodelaceae",
+    "imageSource": "img/aloe.jpg"},
+    {"name": "a jade plant",
+    "family": "Crassulaceae",
+    "imageSource": "img/jade.jpg"},
+    {"name": "the Osage orange",
+    "family": "Moraceae",
+    "imageSource": "img/osage.jpg"},
+    {"name": "a grapevine",
+    "family": "Vitaceae",
+    "imageSource": "img/grapes.jpg"},
+    {"name": "an apple tree",
+    "family": "Rosaceae",
+    "imageSource": "img/apple.jpg"},
+    {"name": "a watermelon plant",
+    "family": "Cucurbitaceae",
+    "imageSource": "img/watermelon.jpg"},
+    {"name": "star fruit tree",
+    "family": "Oxalidaceae",
+    "imageSource": "img/starfruit.jpg"},
 ]
 
 // wrong answers to fill in the other buttons
@@ -35,7 +75,6 @@ let wrongAnswers = [
     "Murphyfus",
     "Pinaceae",
     "Applicaea",
-    "Rosecea",
     "Aquinas",
     "Confucius",
     "Snakephus",
@@ -44,6 +83,13 @@ let wrongAnswers = [
     "Asparagaceae",
     "Arecaceae",
     "Cactaceae",
+    "Papaveraceae",
+    "Poaceae",
+    "Fabaceae",
+    "Myrtaceae",
+    "Salicaceae",
+    "Sapindaceae",
+    "Anacardiaceae",
 ]
 
 // define buttons
@@ -71,13 +117,16 @@ function reset(e) {
 let tenQuestions = []
 
 // get new question, load image, load buttons
+
 function newQuestion() {
+
     // define current plant's attributes
     let randArrIndex = Math.floor(Math.random() * triviaQuestions.length)
     let currentPlant = triviaQuestions[randArrIndex]
     let currentName = currentPlant.name
     let currentImgSrc = currentPlant.imageSource
     let currentFamily = currentPlant.family
+    console.log(`current family: ${currentFamily}`)
 
     // event listener for answerButtons
     answerButtons.addEventListener("click", userClick)
@@ -96,40 +145,48 @@ function newQuestion() {
 
     // place the currentFamily variable randomly in the answerArray
     answerArray[randAnsIndex] = currentFamily
+    console.log(answerArray)
 
     // get array of 4 random wrong answers, 4 because one might be identical to correct answer
-    fourWrongAnswers = fillCurrentWrongAnswers(wrongAnswers)
+    let fourWrongAnswers = fillCurrentWrongAnswers(wrongAnswers)
 
-    // add wrong answers without overwriting the correct answer
-    for (let i = 0; i < answerArray.length; i++) {
-        // add a wrong answer if i is null and i is not the answer
-        // right now this is still adding replicant items
-        if (answerArray[i] == null && answerArray[i] !== currentFamily) {
-             answerArray[i] = fourWrongAnswers[i]
+    // the wrong answers could also be gathered by getting all the family names
+    // and excluding the name of the current plant
+
+    // define array of wrong answers that does not include the answer
+    let filteredWrongAnswers = fourWrongAnswers.filter(str => str != currentFamily)
+
+    // for loop to add filteredWrongAnswers into the null spots
+    for (let i = 0; i < 4; i++) {
+        if (answerArray[i] == currentFamily) {
+            continue
+        } else if (answerArray[i] == null) {
+            answerArray[i] = filteredWrongAnswers.pop()
+            console.log(answerArray)
         }
     }
-
-    // TODO write the above for loop as a recursive function
 
     // populate buttons with the answer array
     fillButtons(answerArray)
-    console.log(answerArray)
 
     // action to take once a user clicks on an answer
     function userClick(e) {
-        const currentButton = document.getElementById(e.target.id)
-        // print which button has been pressed for dev purposes
+        let currentButton = document.getElementById(e.target.id)
         if (e.target.tagName == "BUTTON") {
-            // console.log(`currentButton = ${currentButton.id}`)
             console.log(`currentButton's innerText = ${currentButton.innerText}`)
             if (currentButton.innerText == currentFamily) {
-                alert(`correct: ${currentButton.innerText}!`)
+               alert(`correct: ${currentButton.innerText}!`)
                 scoreNum += 10
                 scoreSpan.innerText = scoreNum
+                // return newQuestion()
+                // for some reason the program is remembering the previous currentFamily
+            } else if (currentButton.innerText != currentFamily) {
+                alert(`Incorrect. The correct answer is ${currentFamily}`)
+                // return newQuestion()
+                }
             }
         }
     }
-}
 
 // fill answers buttons
 function fillButtons(answerArr){
